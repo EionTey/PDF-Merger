88fab49af47631e59eb528083999ae86fdf78d81
Eion Tey <eion.tey@gmail.com>
2023-07-20 19:06:57 +1200

    Simple PDF merger python script

---
+++ pdfMerger.py
@@ -0,0 +1,13 @@
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfMerger()
+
+for file in os.listdir(os.curdir):
+    # Check if PDF
+    if file.endswith(".pdf"):
+        merger.append(file)
+    # Creates combined file
+    merger.write("Combined.pdf")
+
